c191d7d6c73e08691fcbcb7eb4345d4fa60067c9
Kristian Knudsen <48657978+Kristian-Knudsen@users.noreply.github.com>
2023-10-26 10:00:20 +0200

    Kesse/Spellchecker_Tests (#4)

--- src/Spellchecking/test/test_spell_checker.py
+++ src/Spellchecking/test/test_spell_checker.py
@@ -4,16 +4,43 @@ from Spellchecking.spell_checker import SpellChecker
 
 class TestCase(unittest.TestCase):
     """Testing the SpellChecker class"""
-    def test_correct_initialization(self):
-        """ Initializaer function """
-
+    def test_trie_initializes(self):
+        """ Test if the Trie is correctly initialized """
         # Arrange
-        sc = SpellChecker('src/Spellchecking/test/test_wordlist.txt')
+        sc = SpellChecker()
 
         # Act
         # Assert
-        expected = sc.root.char == ""
-        self.assertEqual(expected, expected)
+        actual = sc.root.char == ""
+        self.assertEqual(actual, True)
+
+
+    def test_trie_inserts_correctly(self):
+        """ Test if the insert function works """
+        # Arrange
+        sc = SpellChecker()
+        # Act
+        sc.insert('test')
+        actual = sc.query('t')
+
+        # Assert
+        self.assertEqual(actual[0][0], 'test')
+
+
+    def test_trie_query_correctly(self):
+        """ Test if the query function works """
+        # Arrange
+        sc = SpellChecker()
+        sc.insert('cat')
+        sc.insert('death')
+        sc.insert('deer')
+
+        # Act
+        actual = sc.query('d')
+
+        # Assert
+        self.assertEqual(actual[0][0], 'death')
+
 
 if __name__ == '__main__':
     unittest.main()
