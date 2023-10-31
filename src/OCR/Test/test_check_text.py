ba7dcc223001c2dfb08d5d56092a6165e6d8d558
Malthe Albertsen <msalbertsen7@gmail.com>
2023-10-31 10:14:33 +0100

    fixed pylint errors, but only the ones pertaining to the content made for this branch

--- src/OCR/Test/test_check_text.py
+++ src/OCR/Test/test_check_text.py
@@ -1,30 +1,27 @@
+"""provides unit test functionality"""
 import unittest
-from ocr import ocr_comp
-"""does ocr on a constructed pdf where the text is known and has placed in expected.txt and during the test ocr will done on the pdf and contents will be compared to see if equal"""
 
 def get_word_stream(path):
-    """splits string read into individual words to compare the extracted words themselves"""
-    with open(path, 'r') as file:
+    """splits string read into individual words to compare the extracted words"""
+    with open(path, encoding='utf8', mode='r') as file:
         words = file.read().split()
         return words
 
 class TextExtractionTests(unittest.TestCase):
+    """unit testing class"""
 
     def test_compare_text(self):
         #Arrange
-        """perform ocr on the test image and extracts the words from file created and compare it to the test"""
+        """perform ocr on the test image and extracts the words into an extracted.txt file"""
         #generate a txt file called extracted.txt from test.png here
         extracted_text = get_word_stream('extracted.txt')
         expected_text = get_word_stream('expected.txt')
-        
 
         #Act
-        #read the indvidual words and run them through the spellchecker return the number of correct words
         result = extracted_text == expected_text
 
         #Assert
         self.assertTrue(result, 'the results is false thus the extracted is not correct')
-        
 
 if __name__ == "__main__":
     unittest.main()
