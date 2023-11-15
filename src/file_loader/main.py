3817a9963ae1d3fc679502142fca8cf672c65c9f
JTC2000Official <jonathan.christensen2000@gmail.com>
2023-11-15 20:58:13 +0100

    Lint

--- src/file_loader/main.py
+++ src/file_loader/main.py
@@ -5,6 +5,6 @@ from folder_watcher.folder_watcher import FolderWatcher
 if __name__ == '__main__':
     file_loader = FileLoader()
 
-    folder_watcher = FolderWatcher("watched/file_loader")
+    folder_watcher = FolderWatcher("watched/file_loader", file_loader.readextension)
 
     folder_watcher.watch()
