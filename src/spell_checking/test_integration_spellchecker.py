159bbbecb16b5cd29f8fcda4a6a5af2c30827fd3
Malthe Albertsen <msalbertsen7@gmail.com>
2023-11-21 12:22:42 +0100

    attempting to fix integration test

--- src/spell_checking/test_integration_spellchecker.py
+++ src/spell_checking/test_integration_spellchecker.py
@@ -6,7 +6,7 @@ class SpellcheckerIntegrationTests(unittest.TestCase):
 
     def test_integration_spellchecker(self):
         #Arrange
-        spellchecker = SpellChecker("src/spell_checking/wordlist.txt")
+        spellchecker = SpellChecker("src/spell_checking/wordList.txt")
         text_extractor = TextExtractor()
         with open("src/spell_checking/expected.txt", 'r', encoding="utf-8") as expected_text:
             expected_text = expected_text.read().split()
