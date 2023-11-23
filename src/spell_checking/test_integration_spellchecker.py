71a5a8bb86e53fddbd96130596900f386e1b63b5
Malthe Albertsen <msalbertsen7@gmail.com>
2023-11-23 12:24:03 +0100

    fixed test, passed linter check

--- src/spell_checking/test_integration_spellchecker.py
+++ src/spell_checking/test_integration_spellchecker.py
@@ -1,30 +1,34 @@
+"""Module providing functionaly needed to run integration test"""
 import unittest
 from spell_checking.spell_checker import SpellChecker
 from text_extraction.text_extractor import TextExtractor
 
 class SpellcheckerIntegrationTests(unittest.TestCase):
+    """Class containing the integrationtest between textextractor and spellchecker"""
 
     def test_integration_spellchecker(self):
+        """Method testing if the input of the textextractor cen be received by the spellchecker"""
         #Arrange
         spellchecker = SpellChecker("src/spell_checking/wordList.txt")
         text_extractor = TextExtractor()
-        with open("src/spell_checking/expected.txt", 'r', encoding="utf-8") as expected_text:
-            expected_text = expected_text.read().split()
-            for word in expected_text:
-                word = word.strip
+        expected_text = []
+        with open("src/spell_checking/expected.txt", 'r', encoding="utf-8") as temp_text:
+            temp_text = temp_text.read().split()
+            for word in temp_text:
+                word = word.lower()
+                word = word.strip()
+                expected_text.append(word)
             print(f'Expected text: {expected_text}')
 
         #Act
-        text_extractor.read("src/spell_checking/Test_of_TextExtraction.pdf")
-        spellchecker.handle_files("src/spell_checking/Test_of_TextExtraction.pdf")
-        with open("watched/output/Test_of_TextExtraction.pdf", 'r', encoding="utf-8") as spellchecked_file:
-            spellchecked_file = spellchecked_file.read().split('\n')
+        text_extractor.read("src/spell_checking/Test_File.pdf")
+        spellchecker.handle_files("/watched/spell_checking/Test_File.pdf")
+        with open("/watched/output/Test_File.pdf", 'r', encoding="utf-8") as output:
+            output = output.read().split('\n')
+            spellchecked_file = list(filter(lambda word: word != "", output))
             print(f'Spellchecked text: {spellchecked_file}')
-            if(spellchecked_file == expected_text):
-                status = False
-            else:
-                status = True
-       
+            status = bool(spellchecked_file == expected_text)
+
         #Assert
-        self.assertTrue(status)
+        self.assertTrue(status, "The text was not extracted correctly")
         
