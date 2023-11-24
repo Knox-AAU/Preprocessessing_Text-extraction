851bec5bdaae138739c902240b4d2d1830205954
Mathias Madsen <matmad21@student.aau.dk>
2023-11-24 14:00:53 +0100

    lint and test

--- src/file_loading/test/test_file_loader.py
+++ src/file_loading/test/test_file_loader.py
@@ -1,31 +1,11 @@
 """Module providing a function printing python version."""
 import unittest
+import os
 from ..file_loader import FileLoader
 
 class TestCase(unittest.TestCase):
     """Testing the SpellChecker class"""
-    def test_reads_correct_path(self):
-        """ Test if the readextension function reads the correct path """
-        # Arrange
-        file_loader = FileLoader()
-        file_loader.readextension('src/file_loading/test/test.pdf')
 
-        # Act
-        actual_path = file_loader.path
-        # Assert
-        self.assertEqual(actual_path, 'src/file_loading/test/test')
-
-
-    def test_reads_correct_extension(self):
-        """ Test if the readextension function reads the correct file extension """
-        # Arrange
-        file_loader = FileLoader()
-        file_loader.readextension('src/file_loading/test/test.pdf')
-
-        # Act
-        actual_extension = file_loader.extension
-        # Assert
-        self.assertEqual(actual_extension, '.pdf')
 
 
     # pylint: disable=W0511
@@ -35,47 +15,18 @@ class TestCase(unittest.TestCase):
         """ Test if the openpdf function correctly opens and reads data """
         # Arrange
         file_loader = FileLoader()
-        file_loader.readextension('src/file_loading/test/test.pdf')
-
+        file_loader.path = 'src/file_loading/test/test.pdf'
+        file_loader.output_folder = 'src/file_loading/test/'
         # Act
         file_loader.openpdf()
+        file_found = False
 
-        actual = [
-            file_loader.images[0].format,
-            file_loader.images[0].size,
-            file_loader.images[0].mode,
-            file_loader.images[0].file_name,
-        ]
-        # Assert
-        self.assertListEqual(actual, [
-            'PPM',
-            (1100, 1700),
-            'RGB',
-            'test.pdf'
-        ])
+        if os.path.exists('src/file_loading/test/out_0_test.png'):
+            file_found = True
 
+        #Assert
+        self.assertTrue(file_found)
 
-    def test_opens_image(self):
-        """ Test if the openimage function correctly opens and reads data """
-        # Arrange
-        file_loader = FileLoader()
-        file_loader.readextension('src/file_loading/test/test.jpg')
-
-        # Act
-        file_loader.openimage()
-        actual = [
-            file_loader.images[0].format,
-            file_loader.images[0].size,
-            file_loader.images[0].mode,
-            file_loader.images[0].file_name,
-        ]
-        # Assert
-        self.assertListEqual(actual, [
-            'JPEG',
-            (543, 360),
-            'RGB',
-            'test.jpg'
-        ])
 
 if __name__ == '__main__':
     unittest.main()
