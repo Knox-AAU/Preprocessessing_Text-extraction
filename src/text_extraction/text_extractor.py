f60b3967668a4df0fcd50fc2195ea2e953868bcd
Christian <christianpoulsen2000@gmail.com>
2023-12-01 17:25:23 +0100

    Added Filename, Index, Sentence index, uploader and title

--- src/text_extraction/text_extractor.py
+++ src/text_extraction/text_extractor.py
@@ -12,7 +12,7 @@ class TextExtractor():
         self.out_dir = ""
         self.dpi = 500
 
-    def read(self, input_file):
+    def read(self, input_file, file_name, index, uploader):
         """ Inner function that reads images and outputs the OCR text"""
 
         out_path = f"{self.out_dir}{re.sub(r'[^.]+$', 'txt', os.path.basename(input_file))}"
@@ -23,10 +23,29 @@ class TextExtractor():
 
         text = text.replace("-\n", "")
 
-        # Save each sentence as a new line in the output file
+        # Extract the title from the text (assumes title is at the beginning)
+        title_match = re.search(r'^[^\n]+', text)
+        title = title_match.group(0) if title_match else "No Title Found"
+
+        # Split text into sentences
+        sentences = re.split(
+            r'(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s', text
+        )
+
+        # Save each sentence as a new line in the output file with metadata
         with open(out_path, 'w', encoding='utf-8') as file:
+            # Write File name, Uploader, and Index information once
+            file.write(
+                f"File Name: {file_name}\n"
+                f"Uploader: {uploader}\n"
+                f"Index: {index}\n"
+                f"Title: {title}\n\n"
+            )
+
+            for i, sentence in enumerate(sentences):
+                metadata = f"Sentence Index: {i + 1}\n"
+                file.write(metadata + sentence + '\n')
             print(text)
-            file.write(text)
 
         if os.path.exists(input_file):
             os.remove(input_file)
