5410ba169cb64a26c6bb48e2999eeabad10e79c7
Malthe Albertsen <msalbertsen7@gmail.com>
2023-12-09 11:32:48 +0100

    fixed text_extractor unittest and linter errors

--- src/text_extraction/test/test_check_text.py
+++ src/text_extraction/test/test_check_text.py
@@ -1,5 +1,8 @@
 """provides unit test functionality"""
 import unittest
+import shutil
+import os
+from text_extraction.text_extractor import TextExtractor
 
 def get_word_stream(path):
     """splits string read into individual words to compare the extracted words"""
@@ -14,12 +17,23 @@ class TextExtractionTests(unittest.TestCase):
         """perform ocr on the test image and extracts the words into an extracted.txt file"""
 
         #Arrange
-        #generate a txt file called extracted.txt from test.png here
-        extracted_text = get_word_stream('extracted.txt')
-        expected_text = get_word_stream('expected.txt')
+        src = "src/text_extraction/test/test_files/test.png"
+        dst = "src/text_extraction/test/extracted.png"
+        text_extractor = TextExtractor()
+        text_extractor.out_dir = "src/text_extraction/test/"
+        #copies test file since text extractor deletes files once processed
+        shutil.copy(src, dst)
 
         #Act
+        text_extractor.read("src/text_extraction/test/extracted.png")
+        #convert expected and extracted texts into lists
+        extracted_text = get_word_stream("src/text_extraction/test/extracted.txt")
+        expected_text = get_word_stream("src/text_extraction/test/test_files/expected.txt")
+        #compares extracted text to the expected text to see if extraction was a success
         result = extracted_text == expected_text
+        #deletes the testing file that was copied over
+        if os.path.exists("src/text_extraction/test/extracted.png"):
+            os.remove("src/text_extraction/test/extracted.png")
 
         #Assert
         self.assertTrue(result, 'the results is false thus the extracted is not correct')
