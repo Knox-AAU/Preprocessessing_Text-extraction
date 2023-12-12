8d6ad76b4740f93309ec03263be4fdcd8150fd91
Christian <christianpoulsen2000@gmail.com>
2023-12-12 14:15:29 +0100

    Fixed integration spellchecker

--- src/spell_checking/test/test_integration_spellchecker.py
+++ src/spell_checking/test/test_integration_spellchecker.py
@@ -27,14 +27,6 @@ class SpellcheckerIntegrationTests(unittest.TestCase):
             print(f'Spellchecked text: {output}')
             status = bool(output == expected_text)
 
-        # Print expected and output text
-        print(f'Expected text: {expected_text}')
-        print(f'Output text: {output}')
-
-        # Inspect the content of the output file
-        with open("/watched/output/Test_File.txt", 'r', encoding="utf-8") as output_file:
-            print(f'Output file content: {output_file.read()}')
-
         exclude_words = ["file", "name:", "none", "uploader:", "none", "index:", "none", "title:"]
 
         # Extract relevant text for comparison
