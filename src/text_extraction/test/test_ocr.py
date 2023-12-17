1a637a8a45d0c12d5712b7aa68311f1665b69fb9
Malthe Albertsen <msalbertsen7@gmail.com>
2023-12-17 13:26:38 +0100

    removed test framework from test_ocr.py

--- src/text_extraction/test/test_ocr.py
+++ src/text_extraction/test/test_ocr.py
@@ -1,62 +1,44 @@
-"""Module providing functionaly needed to run test"""
-import unittest
+"""module resposible for testing edgecases for tesseract"""
 import os
 import pytesseract
 from pdf2image import convert_from_path
 
-def chars_to_string(chars):
-    """function to convert a list of chars to a string"""
-    string = ' '.join(chars)
-    return string
+def test_edge_cases():
+    """Method testing proficiency in extraction of different characters"""
+    text =""
+    passed_chars = []
+    declined_chars = []
+    temp_path = "src/text_extraction/test/pdf_page_"
+    ground_truth_path = "src/text_extraction/test/ground_truth.txt"
+    #convert pdf to images and then perform ocr on those images
+    pdf_images = convert_from_path('src/text_extraction/test/Testing_document.pdf')
+    for image in enumerate(pdf_images):
+        image[1].save(temp_path + f"{image[0]+1}.png", 'PNG')
+        text = text + (pytesseract.image_to_string(temp_path + f"{image[0]+1}.png",lang="dan"))
+        os.remove(f"src/text_extraction/test/pdf_page_{image[0]+1}.png")
 
-def log_results(passed_chars, declined_chars, text):
-    """function that creates a log in text file of the test results"""
-    with open("src/text_extraction/test/edge_cases_log.txt", 'w', encoding="utf-8") as log:
-        lines =["passed characters: " + chars_to_string(passed_chars) + "\n",
-                "declined characters: " + chars_to_string(declined_chars) + "\n",
-                f"passed:{len(passed_chars)}" + "|" + f"declined: {len(declined_chars)}"+"\n",
-                "the extracted text: \n", text]
-        log.writelines(lines)
+    #separate words/characters from ground_truth into list
+    with open(ground_truth_path, 'r', encoding="utf-8") as ground_truth:
+        raw_text = ground_truth.read()
+        ground_truth = ground_truth.read().split()
 
-class EdgeCaseTests(unittest.TestCase):
-    """Class containing the tests of edgecases for tesseract"""
+    #if length of text is more than 0 text was succesfully extracted
+    ground_truth_len = len(ground_truth)
+    #iterate all elements in ground_truth and check if that substring is in the extracted text
+    #then append found elements to passed_chars and the ones that weren't to declined_chars
+    for char in range(ground_truth_len):
+        match = text.find(ground_truth[char])
+        if match > -1:
+            passed_chars.append(ground_truth[char])
+        else:
+            declined_chars.append(ground_truth[char])
 
-    def test_edge_cases(self):
-        """Method testing proficiency in extraction of different characters"""
-        #Arrange
-        text =""
-        passed_chars = []
-        declined_chars = []
-        temp_path = "src/text_extraction/test/pdf_page_"
-        ground_truth_path = "src/text_extraction/test/ground_truth.txt"
-        #convert pdf to images and then perform ocr on those images
-        pdf_images = convert_from_path('src/text_extraction/test/Testing_document.pdf')
-        for image in enumerate(pdf_images):
-            image[1].save(temp_path + f"{image[0]+1}.png", 'PNG')
-            text = text + (pytesseract.image_to_string(temp_path + f"{image[0]+1}.png",lang="dan"))
-            os.remove(f"src/text_extraction/test/pdf_page_{image[0]+1}.png")
+    print("passed input: ", passed_chars, "\n")
+    print("declined input: ", declined_chars, "\n")
+    print("passed:", len(passed_chars), "|" + "declined:", len(declined_chars), "\n")
+    print("the expected text: \n" + "------------------\n", raw_text, "\n")
+    print("the extracted text: \n" + "-------------------\n" + text)
 
-        #separate words/characters from ground_truth into list
-        with open(ground_truth_path, 'r', encoding="utf-8") as ground_truth:
-            ground_truth = ground_truth.read().split()
-
-        #Act
-        #if length of text is more than 0 text was succesfully extracted
-        status = len(text) > 0
-        ground_truth_len = len(ground_truth)
-        #iterate all elements in ground_truth and check if that substring is in the extracted text
-        #then append found elements to passed_chars and the ones that weren't to declined_chars
-        for char in range(ground_truth_len):
-            match = text.find(ground_truth[char])
-            if match > -1:
-                passed_chars.append(ground_truth[char])
-            else:
-                declined_chars.append(ground_truth[char])
-
-        chars_evaluated = (len(passed_chars) > 0 or len(declined_chars) > 0)
-        log_results(passed_chars, declined_chars, text)
-
-        #Assert
-        self.assertTrue(status, "Nothing extracted")
-        self.assertTrue(chars_evaluated, "characters have not been evaluated")
+if __name__ == "__main__":
+    test_edge_cases()
         
